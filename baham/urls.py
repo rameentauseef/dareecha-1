2e336f70ca820d62a7364dc861f44c9dfb9e127a
rameentauseef <64321541+rameentauseef@users.noreply.github.com>
2023-05-18 09:13:43 -0700

    completed activity-3

--- baham/urls.py
+++ baham/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from baham import views
+from . import views
+
 
 urlpatterns = [
-    path('', views.view_index, name='home'),
-    path('baham/members/', views.view_members, name='members'),
-    path('baham/vehicles/', views.view_vehicles, name='vehicles'),
-    path('baham/vehicles/create/', views.create_vehicle, name='create_vehicle'),
-    path('baham/vehicles/update/<id>', views.update_vehicle, name='update_vehicle'),
-    path('baham/vehicles/create/save/', views.save_vehicle, name='save_vehicle'),
-    path('baham/aboutus/', views.view_aboutus, name='aboutus')
+    path('', views.view_home, name='home'),
+    path('baham/vehicles', views.view_vehicles, name='vehicles'),
+    path('baham/vehicles/create', views.create_vehicle, name='createvehicle'),
+    path('baham/vehicles/save/', views.save_vehicle, name='savevehicle'),
+    path('baham/aboutus', views.view_aboutus, name='aboutus'),
+    path('veh_delete/<int:pk>/void/', views.veh_delete, name='veh_delete'),
+    path('veh_nodelete/<int:pk>/unvoid/', views.veh_nodelete, name='veh_nodelete'),
 ]
